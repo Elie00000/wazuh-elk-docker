9e87659ebb204b0b2215753418e92b01900460d9
Jerome <133158597+Elie00000@users.noreply.github.com>
2025-11-17 11:57:05 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-# Démarrer les conteneurs avec Docker Compose
+sudo curl -o logstash/templates/wazuh.json https://packages.wazuh.com/integrations/elastic/4.x-8.x/dashboards/wz-es-4.x-8.x-template.json
+
 docker-compose up -d
