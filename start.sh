945da816767d97c2605b71eeea57cd73163103cd
Jerome <133158597+Elie00000@users.noreply.github.com>
2025-11-17 15:53:09 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 sudo curl -o logstash/templates/wazuh.json https://packages.wazuh.com/integrations/elastic/4.x-8.x/dashboards/wz-es-4.x-8.x-template.json
 
-docker-compose up -d
+docker compose up -d
